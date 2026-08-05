2e4c843cccd11feba7ed2f267608664006be5b73
Martin Saturka <da42eb2ec67a4355@tangloid.net>
2026-08-05 21:48:01 +0200

    updated the form of Uvicorn setting

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,10 +3,9 @@
 # Start the nginx reverse proxy
 service nginx start
 
+export FORWARDED_ALLOW_IPS="${FORWARDED_ALLOW_IPS:-*}"
+export UVICORN_FORWARDED_ALLOW_IPS="$FORWARDED_ALLOW_IPS"
+export UVICORN_PROXY_HEADERS=1
+
 # Launch vLLM engine, bound locally on internal port 8001
-exec vllm serve "$VLLM_MODEL_NAME" \
-    --proxy-headers \
-    --forwarded-allow-ips "${FORWARDED_ALLOW_IPS:-*}" \
-    --host 127.0.0.1 \
-    --port 8001 \
-    "$@"
+exec vllm serve "$VLLM_MODEL_NAME" --host 127.0.0.1 --port 8001 "$@"
