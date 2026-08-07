c8817585d402a8bf4bf5bf8368978c7eea5ec701
Martin Saturka <da42eb2ec67a4355@tangloid.net>
2026-08-07 18:35:54 +0200

    shuffled input of vLLM params

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,8 @@
 service nginx start
 
 export FORWARDED_ALLOW_IPS="${FORWARDED_ALLOW_IPS:-*}"
-export UVICORN_FORWARDED_ALLOW_IPS="$FORWARDED_ALLOW_IPS"
+export UVICORN_FORWARDED_ALLOW_IPS="${FORWARDED_ALLOW_IPS}"
 export UVICORN_PROXY_HEADERS=1
 
 # Launch vLLM engine, bound locally on internal port 8001
-exec vllm serve "$VLLM_MODEL_NAME" --host 127.0.0.1 --port 8001 "$@"
+exec vllm serve "$@" --host 127.0.0.1 --port 8001
